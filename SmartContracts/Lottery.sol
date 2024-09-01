dd0771ae2e0cf314ab12a310013a73b176f243e5
Hafiz Burhan <87270952+Itshbhere@users.noreply.github.com>
2024-09-01 22:17:33 +0500

    Lottery Contract

---
+++ SmartContracts/Lottery.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Lottery {
+
+    private 
+    struct LotteryDetails 
+
+    function createLottery(uint256 TicketPrice, uint256 TimeSpan) public {
+
+
+    }
+}
